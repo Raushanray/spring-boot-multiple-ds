8012070c5dfd4fad27261815578813fa518fa3db
Raushanray <raushankuray1998@gmail.com>
2023-08-25 12:57:49 +0530

    Create a spring boot project for work with multiple database with single spring boot application..

---
+++ src/main/java/com/lcwr/db/api/SpringBootMultipleDsApplication.java
@@ -0,0 +1,13 @@
+package com.lcwr.db.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleDsApplication.class, args);
+	}
+
+}
